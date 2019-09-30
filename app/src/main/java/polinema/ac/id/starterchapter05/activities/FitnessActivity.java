f1a11cccde8dbe08b89225be43968468c847fd13
RAHADIAN <rahadian_98@yahoo.co.id>
2019-10-01 00:22:54 +0700

    pushups and dips

--- app/src/main/java/polinema/ac/id/starterchapter05/activities/FitnessActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/FitnessActivity.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 
 import polinema.ac.id.starterchapter05.R;
+import polinema.ac.id.starterchapter05.fragments.DipsFragment;
 import polinema.ac.id.starterchapter05.fragments.HandFragment;
+import polinema.ac.id.starterchapter05.fragments.PushupFragment;
 
 public class FitnessActivity extends AppCompatActivity {
 
@@ -30,5 +32,33 @@ public class FitnessActivity extends AppCompatActivity {
             fragmentTransaction.commit();
         }
 
+    }
+
+    public void handlerDips(View view) {
+        DipsFragment dipsFragment = (DipsFragment) getSupportFragmentManager().findFragmentByTag("Dips_Fragment");
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        if(dipsFragment != null && dipsFragment.isVisible()) {
+            fragmentTransaction.commit();
+        }
+        else {
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+            fragmentTransaction.replace(R.id.place_fragment_here, new DipsFragment(), "Dips_Fragment");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
+    }
+
+    public void handlerPushup(View view) {
+        PushupFragment pushupFragment = (PushupFragment) getSupportFragmentManager().findFragmentByTag("Pushup_Fragment");
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        if(pushupFragment != null && pushupFragment.isVisible()) {
+            fragmentTransaction.commit();
+        }
+        else {
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+            fragmentTransaction.replace(R.id.place_fragment_here, new PushupFragment(), "Pushup_Fragment");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
     }
 }
